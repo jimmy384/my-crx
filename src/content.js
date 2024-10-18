987c5d81afad23a0db777c909c1edc987f792aaf
jimmy <280376881@qq.com>
2024-10-18 23:37:53 +0800

    1

--- src/content.js
+++ src/content.js
@@ -16,4 +16,43 @@ $(document).ready(() => {
     } else {
         console.log("没有找到输入框")
     }
+
+    // const nameInput = findNearbyElement("昵称", 'input')
+    // console.log(nameInput)
+    // console.log(findNearbyElement("我的签名", 'textarea'))
+    // console.log(findNearbyElement("出生日期", 'input'))
+    // console.log(findNearbyElement("用户名", 'span'))
+
+    // setElementValue(nameInput, "jimmy")
+
+
+    function findNearbyElement(labelText, tagType) {
+        const label = $(`*:contains('${labelText}')`)
+            .filter((index, item) => {
+                return $(item).children().length === 0
+            })
+            .first()
+        return doFindNearbyElement(label, tagType, 1)
+    }
+
+    function doFindNearbyElement(fromElement, tagType, depth) {
+        if (depth > 3) {
+            return null
+        }
+        const element = fromElement.nextAll().find(tagType).first()
+        if (element.length === 0) {
+            const newFromElement = fromElement.parent()
+            if (newFromElement.length === 0) {
+                return null
+            } else {
+                return doFindNearbyElement(newFromElement, tagType, depth + 1)
+            }
+        } else {
+            return element
+        }
+    }
+
+    function setElementValue(element, value) {
+        $(element).val(value).trigger('input');
+    }
 });
