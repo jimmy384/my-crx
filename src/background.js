633f0d6e53253739998775e2b643e284715ef2cf
jimmy <280376881@qq.com>
2024-10-20 19:18:38 +0800

    1

--- src/background.js
+++ src/background.js
@@ -1,4 +1,18 @@
-// 读取配置参数
+import { AxiosError } from 'axios';
+import httpRequest from './common/httpRequest.js'
+
+// 
+/**
+ * 读取配置参数
+ * 入参:
+ * {
+ *     action: "getConfig"
+ * }
+ * 返回值：
+ * {
+ *     key1: value1
+ * }
+ */
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "getConfig") {
         chrome.storage.local.get(null, result => {
@@ -8,39 +22,130 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     return true;
 });
 
-// 保存配置参数
+/**
+ * 保存配置参数
+ * 入参:
+ * {
+ *     action: "saveConfig",
+ *     settings: {
+ *         key1: value1
+ *     }
+ * }
+ * 返回值：
+ * {
+ *     success: true
+ * }
+ */
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "saveConfig") {
         chrome.storage.local.set(message.settings, () => {
             console.log("配置参数已保存");
-            sendResponse({ "status": "success" });
+            sendResponse({ "success": true });
         });
     }
     return true;
 });
 
+/**
+ * 发送请求
+ * 入参:
+ * {
+ *     action: "sendRequest",
+ *     method: "POST",
+ *     path: "/api/v1/user",
+ *     params: {
+ *         "id": 123    
+ *     },
+ *     data: {},
+ *     config: {
+ *         headers: {
+ *             "Authorization": "Bearer xxx"
+ *         },
+ *         ...其他axios配置
+ *     }
+ * }
+ * 返回值：
+ * {
+ *     success: true,
+ *     data: {},
+ *     errCode: "", // 客户端本地发生错误时, errCode=ClientError, 服务端发生错误时, 错误码由后端给出
+ *     errMsg: ""
+ * }
+ */
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "forwardRequest") {
+    if (message.action === "sendRequest") {
         chrome.storage.local.get(null, result => {
+            const method = message.method
             const url = result.backendUrl + message.path
-            fetch(url, {
-                method: message.method,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(message.param),
-            }).then(response => {
-                if (response.status !== 200) {
-                    throw new Error(`请求失败, 响应码:${response.status}`)
-                }
-                return response.json()
-            }).then(data => {
-                console.log("处理接口返回数据", data);
-                sendResponse(data);
-            }).catch(error => {
-                console.error('请求出错:', error);
-            });
+            if (method === "GET") {
+                httpRequest.get(url, message.params, message.config)
+                    .then(body => {
+                        sendResponse(body)
+                    }, body => {
+                        handleError(body, sendResponse) 
+                    })
+            } else if (method === "POST") {
+                httpRequest.post(url, message.params, message.data, message.config)
+                    .then(body => {
+                        sendResponse(body)
+                    }, body => {
+                        handleError(body, sendResponse) 
+                    })
+            } else if (method === "PUT") {
+                httpRequest.put(url, message.params, message.data, message.config)
+                    .then(body => {
+                        sendResponse(body)
+                    }, body => {
+                        handleError(body, sendResponse) 
+                    })
+            } else if (method === "DELETE") {
+                httpRequest.del(url, message.params, message.data, message.config)
+                    .then(body => {
+                        sendResponse(body)
+                    }, body => {
+                        handleError(body, sendResponse) 
+                    })
+            } else {
+                sendResponse({
+                    "success": false,
+                    "errCode": "ClientError",
+                    "errMsg": `background script的sendRequest不支持${httpRequest.method}请求方法`
+                })
+            }
+
+            // fetch(url, {
+            //     method: message.method,
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify(message.param),
+            // }).then(response => {
+            //     if (response.status !== 200) {
+            //         throw new Error(`请求失败, 响应码:${response.status}`)
+            //     }
+            //     return response.json()
+            // }).then(data => {
+            //     console.log("处理接口返回数据", data);
+            //     sendResponse(data);
+            // }).catch(error => {
+            //     console.error('请求出错:', error);
+            // });
         });
     }
     return true;
 })
+
+function handleError(body, sendResponse) {
+    let errorResult;
+    if (body instanceof AxiosError) {
+        errorResult = {
+            success: false,
+            errCode: "ClientError",
+            errMsg: body.message,
+            data: body
+        }
+    } else {
+        errorResult = body
+    }
+    sendResponse(errorResult);
+}
--- src/content.js
+++ src/content.js
@@ -8,16 +8,22 @@ $(document).ready(() => {
     // 使用jQuery找到页面的第一个输入框
     var firstInput = $('input:first')
     if (firstInput.length) {
-        firstInput.css('border', '2px solid red'); // 给它添加一个红色边框以示识别
+        // 给它添加一个红色边框以示识别
+        firstInput.css('border', '2px solid red')
         console.log("找到的第一个输入框: ", firstInput)
-
-        const message = { action: "forwardRequest", path: "/test", method: "POST", param: { "name": "jimmy" } }
-        chrome.runtime.sendMessage(message, response => {
-            console.log("保存设置结果:", response)
-        })
     } else {
         console.log("没有找到输入框")
     }
+
+    const message = {
+        action: "sendRequest",
+        method: "POST",
+        path: "/test",
+        params: { "name": "jimmy" }
+    }
+    chrome.runtime.sendMessage(message, response => {
+        console.log("请求结果:", response)
+    })
 
     start()
 
@@ -39,6 +45,6 @@ $(document).ready(() => {
             console.log("查找相关元素失败")
             alert("查找相关元素失败")
         }
-        
-    } 
+
+    }
 });
